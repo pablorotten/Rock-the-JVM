d92c763b9fd3d8ead3026cd5e81ebb463133f1d2
Pablo <pablorottendev@gmail.com>
2019-05-06 00:59:42 +0200

    3. Expressions

---
+++ rock-the-jvm-scala-begginers/src/lectures/part1basics/Expressions.scala
@@ -0,0 +1,66 @@
+package lectures.part1basics
+
+object Expressions extends App {
+
+  val x = 1 + 2 // EXPRESSION
+  println(x)
+
+  println(2 + 3 * 4)
+  // + - * / & | ^ << >> >>> (right shift with zero extension)
+
+  println(1 == x)
+  // == != > >= < <=
+
+  println(!(1 == x))
+  // ! && ||
+
+  var aVariable = 2
+  aVariable += 3 // also works with thq -= *= /= ..... side effects
+  println(aVariable)
+
+  // IF expression
+  val aCondition = true
+  val aConditionedValue = if(aCondition) 5 else 3 // IF EXPRESSION
+  println(aConditionedValue)
+  println(if(aCondition) 5 else 3)
+  println(1 + 3)
+
+  var i = 0
+  var aWhile = while(i < 10) {
+    println(i)
+    i += 1
+  }
+
+  // NEVER WRITE THIS AGAIN.
+
+  // EVERYTHING in Scala is an Expression!
+
+  val aWeirdValue = (aVariable = 3) // Unit == void
+  println(aWeirdValue)
+
+  // side effects: println(), whiles, reassigning
+
+  // Code blocks
+
+  val aCodeBlock = {
+    val y = 2
+    val z = y +1
+
+    if (z > 2) "hello" else "goodbye"
+  }
+
+  // 1. difference between "hello world" vs println("hello world")?
+  // "hello world" is a value of String and println("hello wolrd") is a Expression which is a side effect that prints a String
+  // 2.
+
+  val someValue = {
+    2 < 3
+  }
+  // someValue = true
+
+  val someOtherValue = {
+    if (someValue) 239 else 986
+    42
+  }
+  // someOtherValue = 42
+}
